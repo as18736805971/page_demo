2bf873c77b7a798dce3367cd2a1a0658b6ac02de
stars <as18736805971@gmail.com>
2022-09-16 17:14:12 +0800

    ++

--- src/views/image.vue
+++ src/views/image.vue
@@ -36,67 +36,8 @@ export default {
         quality: 0.75
       })
       console.log(aa, '压缩后')
-      this.imageUrl = aa
       return false
-      // console.log(file, 'file')
-      // let _this = this
-      // return new Promise((resolve, reject) => {
-      //   let isLt2M = file.size / 1024 / 1024 < 1 // 判定图片大小是否小于10MB
-      //   if(!isLt2M) {
-      //     reject()
-      //   }
-      //   let image = new Image(), resultBlob = '';
-      //   image.src = URL.createObjectURL(file);
-      //   image.onload = () => {
-      //     // 调用方法获取blob格式，方法写在下边
-      //     resultBlob = _this.compressUpload(image, file);
-      //     console.log(resultBlob, 'resultBlob')
-      //     this.toFormData(resultBlob)
-      //     resolve(resultBlob)
-      //   }
-      //   image.onerror = () => {
-      //     reject()
-      //   }
-      // })
     },
-    /* 图片压缩方法-canvas压缩 */
-    compressUpload(image, file) {
-      let canvas = document.createElement('canvas')
-      let ctx = canvas.getContext('2d')
-      let { width } = image, { height } = image
-      canvas.width = width
-      canvas.height = height
-      ctx.fillRect(0, 0, canvas.width, canvas.height)
-      ctx.drawImage(image, 0, 0, width, height)
-      // 进行最小压缩0.1
-      let compressData = canvas.toDataURL(file.type || 'image/jpeg', 0.6)
-      // 压缩后调用方法进行base64转Blob，方法写在下边
-      this.imageUrl = compressData
-      let blobImg = this.dataURItoBlob(compressData)
-      return blobImg
-    },
-    /* base64转Blob对象 */
-    dataURItoBlob(data) {
-      let byteString;
-      if(data.split(',')[0].indexOf('base64') >= 0) {
-        byteString = atob(data.split(',')[1])
-      }else {
-        byteString = unescape(data.split(',')[1])
-      }
-      let mimeString = data
-          .split(',')[0]
-          .split(':')[1]
-          .split(';')[0];
-      let ia = new Uint8Array(byteString.length)
-      for( let i = 0; i < byteString.length; i += 1) {
-        ia[i] = byteString.charCodeAt(i)
-      }
-      return new Blob([ia], {type: mimeString})
-    },
-    toFormData(imageBlob) {
-      let fileOfBlob = new File([imageBlob], '001.jpg'); // 重命名了
-      console.log(fileOfBlob, 'fileOfBlob')
-    }
   },
 }
 </script>
