83a56acbbd700f84c2ccfadbbf2217f93c093ae4
stars <as18736805971@gmail.com>
2023-09-13 18:15:30 +0800

    ++

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,8 @@ const resolve = (dir) => {
 
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+const Timestamp = new Date().getTime()
+
 module.exports = defineConfig({
   publicPath: '/',                  // 部署应用包时的基本 URL /admin 打包后文件路径会增加/admin
   outputDir: 'dist',                // build 时生成的生产环境构建文件的目录
@@ -14,12 +16,17 @@ module.exports = defineConfig({
   indexPath: 'index.html',          // build 时指定生成的 index.html 的输出路径
   transpileDependencies: true,      // 忽略所有 node_modules 中的文件
   lintOnSave: false,                // 是否在开发环境下通过 eslint-loader 在每次保存时 lint 代码
+  productionSourceMap: false,
   configureWebpack: {
     name: '演示项目',                 // 页面名称
     resolve: {
       alias: {
         '@': resolve('src')      // 文件快捷路径
       }
+    },
+    output: {
+      filename: 'static/js/[name].' + Timestamp + '.js',
+      chunkFilename: 'static/js/[name].' + Timestamp + '.js'
     },
     plugins:[
       new CopyWebpackPlugin([
