76ae299216f22a0783b08e3f84e15cbcac1a406f
stars <as18736805971@gmail.com>
2025-05-15 18:12:47 +0800

    福田跑分

---
+++ src/views/demo/demo11.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="amap-page-container">
+    <el-amap
+      ref="map"
+      :zoom="zoom"
+      :center="center"
+      class="amap-demo"
+    />
+  </div>
+</template>
+
+<script>
+import {nextTick} from "vue/src/core/util";
+
+export default {
+  name: 'amap-page',
+  data() {
+    return {
+      zoom: 10,
+      map: null,
+      center: [115.167331, 34.641122]
+    }
+  },
+  created() {
+    // this.getLocation()
+  },
+  mounted() {
+    this.nextTick(() => {
+      this.init()
+    })
+  },
+  methods: {
+    init() {
+      this.map = this.$refs.map.$$getInstance()
+      console.log(this.map)
+    }
+  }
+}
+</script>
+
+<style>
+.amap-page-container {
+  width: 100%;
+  height: 100vh;
+}
+</style>
